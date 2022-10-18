cd12eb67051d41f86eb522c0a5d0b2c8268b3981
MasilaDavid <davidmasila97@gmail.com>
2022-10-18 10:51:57 +0300

    API'

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#This script uses requests on a REST API to gather data about employee and thier tasks
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    end_point = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(end_point + 'users/{}'.format(user_id)).json()
+    todos = requests.get(end_point + 'todos?userId={}'.format(user_id)).json()
+    completed = []
+    for todo in todos:
+        if todo.get('completed') == True:
+            completed.append(todo)
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), len(completed), len(todos)))
+    for task in completed:
+        print("\t{}".format(task.get('title')))
