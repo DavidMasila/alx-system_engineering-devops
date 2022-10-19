dcf6f62b52da8877a4a76d4df4420b20865f095a
MasilaDavid <davidmasila97@gmail.com>
2022-10-19 11:06:16 +0300

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-"""Using what you did in the task #0, extend your Python script to export data in the CSV format.
-
-Requirements:
-
-Records all tasks that are owned by this employee
-Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv
-"""
+"""Using what you did in the task #0, extend your Python script to export data in the CSV format"""
 
 from csv import DictWriter, QUOTE_ALL
 from requests import get
