1645435cbf1895d76b22f4570cbcab621dec4393
MasilaDavid <davidmasila97@gmail.com>
2022-10-19 11:02:59 +0300

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-"""Uses requests to gather employee ID
-and Todo items from REST API
-Then exports the information to CSV
+"""Using what you did in the task #0, extend your Python script to export data in the CSV format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
 """
 
 from csv import DictWriter, QUOTE_ALL
