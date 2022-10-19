29d62dc39e42886c56a93979a3f6d406f1a64efb
MasilaDavid <davidmasila97@gmail.com>
2022-10-19 12:06:14 +0300

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API, for a given employee ID, returns information
+Using a REST API, for a given employee ID and returns information
 about his/her TODO list progress.
 """
+import requests
+import sys
 
-from requests import get
-from sys import argv
 
 if __name__ == "__main__":
-    """Get id from the console"""
-    id = int(argv[1])
-    emp_url = f'https://jsonplaceholder.typicode.com/users/{id}'
-    task_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
 
-    emp_details = get(emp_url).json()
-    total_tasks = get(task_url).json()
+    id_user = int(sys.argv[1])
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
 
-    completed_tasks = []
-    for todo in total_tasks:
-        if todo.get('completed') == True:
-            completed_tasks.append(todo)
+    req_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    req_user = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
 
-    print(
-        f"Employee {emp_details.get('name')} is done with tasks({len(completed_tasks)}/{len(total_tasks)})")
-    for todos in completed_tasks:
-        print("\t{}".format(todos.get('title')))
+    print('Employee ', end='')
+
+    for i in req_user:
+        if id_user == i.get('id'):
+            EMPLOYEE_NAME = i.get('name')
+
+    for j in req_todos:
+        total_task = j.get('userId')
+        if total_task == id_user:
+            TOTAL_NUMBER_OF_TASKS += 1
+
+            number_task = j.get('completed')
+            if number_task is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(j.get('title'))
+
+    print('{} is done with tasks({}/{}):'.format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for j in TASK_TITLE:
+        print('\t {}'.format(j))
